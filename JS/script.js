cb5c8789bf221eceb435aaf2f133ff48ac4ac38b
Denis Kamchatov <kamcatovdenis@gmail.com>
2021-02-27 16:16:04 +0300

    Create script.js

---
+++ JS/script.js
@@ -0,0 +1,12 @@
+function rowReverse() {
+    document.querySelector(".container__row").style.flexDirection = "row-reverse"
+}
+
+function columnReverse() {
+    document.querySelector(".container__column").style.flexDirection = "column-reverse"
+}
+
+function orderChange() {
+    document.querySelector('.container__form_image').style.order = "2"
+    console.log(123)
+}
